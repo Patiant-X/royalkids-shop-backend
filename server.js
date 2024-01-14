2c3e31c7243ba79596d1d6dd2ef0a2a58e53b02c
PATIANT_X <ngwenyathabani080@gmail.com>
2024-01-15 01:50:24 +0200

    changered deployed fronted url

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ app.use(express.urlencoded({ extended: true }));
 //Cookie parser middleware
 app.use(cookieParser());
 app.use(cors({
-  origin: ["http://localhost:3000", "https://royalkids-shop.vercel.app"],
+  origin: ["http://localhost:3000", "https://royalkids-shop.vercel.app/"],
   credentials: true,
 }))
 
