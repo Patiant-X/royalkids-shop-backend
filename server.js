f4da22b11b9ef9a7ef4be5c9d1433becbbb8cbf8
PATIANT_X <ngwenyathabani080@gmail.com>
2024-01-15 02:00:52 +0200

    changed server.js frontend url

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(cors({
   origin: ["http://localhost:3000", "https://royalkids-shop.vercel.app/"],
-  credentials: true,
+  credentials: true,                 
 }))
 
 app.get("/", (req, res) => {
